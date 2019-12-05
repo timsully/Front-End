d944477daee0b5b20b5bc7d113f4ffb2794dea48
Tim Sullivan <timsullivan97@gmail.com>
2019-12-04 23:31:24 -0800

    add foundation for react app pre babel

---
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is running');
+
+// JSX - JavaScript XML (Extension of JS language)
+var template = <p>This is JSX from app.js!</p>;
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot);
